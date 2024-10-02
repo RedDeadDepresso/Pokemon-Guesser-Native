63190f47b0d60d4854eaa30e93dbd6feca114099
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2024-10-02 21:13:58 +0100

    feat: index page

---
+++ app/(tabs)/quiz.tsx
@@ -0,0 +1,18 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { ThemedView } from '@/components/ThemedView'
+import { ThemedText } from '@/components/ThemedText'
+
+const Quiz = () => {
+  return (
+    <ThemedView>
+      <ThemedText>Quiz</ThemedText>
+    </ThemedView>
+  )
+}
+
+const styles = {
+
+}
+
+export default Quiz
